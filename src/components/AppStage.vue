faa4f28f6d5839ff466376a855b781f64761c0ee
drinkingtheink <jason.m.harrison@gmail.com>
2023-09-08 23:09:55 -0500

    user input progress

--- src/components/AppStage.vue
+++ src/components/AppStage.vue
@@ -3,8 +3,24 @@
     <h1>Shape-Scaper</h1>
     <section class="settings">
       <h2>Settings:</h2>
+      <div class="setting circle-count">
+        <label for="circle-count-input"># of Circles:</label>
+        <input value="10" id="circle-count-input" type="number" max="100" />
+      </div>
+      <div class="setting tri-count">
+        <label for="tri-count-input"># of Triangles:</label>
+        <input value="10" id="tri-count-input" type="number" max="100" />
+      </div>
+      <div class="setting rect-count">
+        <label for="rect-count-input"># of Rectangles:</label>
+        <input value="10" id="rext-count-input" type="number" max="100" />
+      </div>
     </section>
-    <Shaper/>
+    <Shaper
+      :circCount="circCount"
+      :rectCount="rectCount"
+      :triCount="triCount"
+    />
   </div>
 </template>
 
@@ -18,6 +34,13 @@ export default {
   },
   props: {
     msg: String
+  },
+  data() {
+    return {
+      circCount: 0,
+      triCount: 0,
+      rectCount: 0,
+    }
   }
 }
 </script>
